db2c8e6e90cff9f0a7c4450ce9356eec556c1e73
Bhupendr1 <bhupendranirwan737@gmail.com>
2023-06-05 09:20:15 +0530

    05-06-2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'switchpro';
+  Selectedproduct:any;
+  Data(event:any){
+this.Selectedproduct=event.target.value
+  }
 }
